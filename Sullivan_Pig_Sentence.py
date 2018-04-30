caf18de8fb2ee6444cdabb16183e9626a47044c2
Ali <aliimody1@gmail.com>
2018-04-30 07:48:30 -0400

    Add files via upload

---
+++ Sullivan_Pig_Sentence.py
@@ -0,0 +1,18 @@
+#Shaan Sullivan
+#This program converts sentences to pig latin
+
+#Initialize variables
+latin = ""
+sentence = ''
+latin_list = []
+
+#Prints the sentence in pig latin
+while sentence != "stop":
+    sentence = input("Enter a sentence to translate it to pig latin: ")
+    if sentence != 'stop':
+        latin_list = sentence.split(" ")
+        for i in latin_list:
+            latin += i[1:] + i[0] + "ay "
+        print(latin)
+    elif sentence == "stop":
+        print("Job done.")
